b23c15da6b0807adbd95d659bcfd694b6a0a1bc4
MNelson1998 <106958476+MNelson1998@users.noreply.github.com>
2022-06-17 14:09:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.multiselect ('What fruit would you like to add?' 'jackfruit')
+add_my_fruit = streamlit.multiselect ('What fruit would you like to add?', 'jackfruit')
 streamlit.write ('Thanks for adding', add_my_fruit)
